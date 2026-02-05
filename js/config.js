50b1c3fa039922886aedc559138044f63a881439
Mauryan04 <102226541+Mauryan04@users.noreply.github.com>
2026-02-05 14:19:10 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_Hmfmk0hhu', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5hs9294i0nrjhj4t2jkkglfste', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3vkmgo5g70s0lp90j7b3g8v7mj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://ah93yq8y22.execute-api.us-east-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
